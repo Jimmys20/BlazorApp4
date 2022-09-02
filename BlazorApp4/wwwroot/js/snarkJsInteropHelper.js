dfd413dcc592809adc00d844b571409f554f7aa4
jimmys20 <maragkosd12@gmail.com>
2022-09-02 21:50:19 +0300

    Add project files.

---
+++ BlazorApp4/wwwroot/js/snarkJsInteropHelper.js
@@ -0,0 +1,6 @@
+﻿
+
+export async function calculateProof() {
+    const { proof, publicSignals } =
+        await snarkjs.groth16.fullProve({ a: 3, b: 11 }, "circuit.wasm", "circuit_final.zkey");
+}
